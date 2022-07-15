8217d57dc1c204b5ad7a91544de20014d9d1fff5
Parisa Z <92223059+pzakeri22@users.noreply.github.com>
2022-07-15 14:59:49 +0100

    Updated mouseovers

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 
 let hero = document.querySelector('.hero');
-let skills = document.querySelector('.skills');
-let about = document.querySelector('.about');
-// let portfolio = document.querySelector('.portfolio');
 
  function dashedBorderOn(event) {
     event.target.style.border = '2px dotted grey';
-
- }
-
- function dashedBorderOff(event) {
-    event.target.style.border = '' ;
  }
 
 hero.addEventListener('mouseover', dashedBorderOn);
 hero.addEventListener('mouseout', dashedBorderOff);
-skills.addEventListener('mouseover', dashedBorderOn);
-skills.addEventListener('mouseout', dashedBorderOff);
-about.addEventListener('mouseover', dashedBorderOn);
-about.addEventListener('mouseout', dashedBorderOff);
-// portfolio.addEventListener('mouseover', dashedBorderOn);
-// portfolio.addEventListener('mouseout', dashedBorderOff);
+
 
